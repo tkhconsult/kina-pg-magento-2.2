3883bc7d8f93a813fbf0e49159051ed5db2fc8be
kch <wilzokch@gmail.com>
2021-04-28 14:42:10 +0800

    add more debug log & do not check transaction type

--- Helper/Gateway/Loader.php
+++ Helper/Gateway/Loader.php
@@ -41,6 +41,7 @@ class Loader {
     protected $backRefUrl;
 
     const TRANSACTION_TYPE_CHARGE = 'order';
+    const TRANSACTION_TYPE_PAYMENT = 'payment';
     const TRANSACTION_TYPE_AUTHORIZATION = 'authorize';
 
     /**
@@ -99,7 +100,7 @@ class Loader {
      * @param $bankResponse
      * @return bool
      */
-    public function checkTransaction($order, $bankResponse) {
+    public function checkTransaction($order, $bankResponse, $logger = null) {
         $amount   = $bankResponse->{Response::AMOUNT};
         $currency = $bankResponse->{Response::CURRENCY};
         $trxType  = $bankResponse::TRX_TYPE;
@@ -108,18 +109,24 @@ class Loader {
         $order_currency = $order->getOrderCurrencyCode();
 
         //Validate currency
-        if(strtolower($currency) !== strtolower($order_currency))
+        if(strtolower($currency) !== strtolower($order_currency)) {
+            if(!is_null($logger)) $logger->error('currency not matched: ' . strtolower($currency) . '!==' . strtolower($order_currency));
             return false;
+        }
 
         //Validate amount
-        if($amount <= 0)
+        if($amount <= 0) {
+            if(!is_null($logger)) $logger->error('amount less than 0: ' . $amount);
             return false;
+        }
 
         if($trxType === KinaBankGateway::TRX_TYPE_REFUND)
             return $amount <= $order_total;
 
         if($trxType === KinaBankGateway::TRX_TYPE_REVERSAL)
             return $amount <= $order_total;
+
+        if(!is_null($logger)) $logger->info('amount: "' . $amount . '", "' . $order_total . '"');
 
         return $amount == $order_total;
     }
