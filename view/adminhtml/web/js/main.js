840d1a7003886bb12e8c64f1c719ec8412dae1e9
kch <wilzokch@gmail.com>
2021-09-07 21:44:06 +0800

    show both test & prod url

--- view/adminhtml/web/js/main.js
+++ view/adminhtml/web/js/main.js
@@ -2,13 +2,7 @@ require(["jquery"],
   function($){
     $(document).ready(function() {
       $('#payment_us_kinabank_gateway_prod_url').prop('disabled', true);
-      $('#payment_us_kinabank_gateway_test_mode').change(function() {
-        if($(this).val() == '1') {
-          $('#payment_us_kinabank_gateway_prod_url').val('test');
-        } else {
-          $('#payment_us_kinabank_gateway_prod_url').val('prod');
-        }
-      }).trigger('change');
+      $('#payment_us_kinabank_gateway_test_url').prop('disabled', true);
     })
   }
-);
+)
